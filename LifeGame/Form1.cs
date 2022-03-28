a27ced9d0cbc5643414db1ed461e83062eead313
KateKrechet <k.krechet@yandex.ru>
2022-03-28 21:20:56 +0300

    Add project files.

---
+++ LifeGame/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LifeGame
+{
+    public partial class Form1 : Form
+    {
+        int N = 5;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void CreateGrid(int n)
+        {
+            dataGridView1.ColumnCount = n;
+            dataGridView1.RowCount = n;
+            int w = dataGridView1.Width > dataGridView1.Height ? dataGridView1.Height :
+                dataGridView1.Width;
+            for(int i=0;i<n;i++)
+            {
+                dataGridView1.Rows[i].Height = w / n;
+                dataGridView1.Columns[i].Width = w / n;
+            }
+        }
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            N = trackBar1.Value;
+            CreateGrid(N);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            CreateGrid(N);
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int col = e.ColumnIndex;
+            int row = e.RowIndex;
+            if (dataGridView1[col, row].Style.BackColor == Color.Black)
+                dataGridView1[col, row].Style.BackColor = Color.White;
+            else
+                dataGridView1[col, row].Style.BackColor = Color.Black;
+            dataGridView1.ClearSelection();//убрать выделение
+
+        }
+
+        
+    }
+}
---
+++ LifeGame/LifeGamecs.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace LifeGame
+{
+    class LifeGamecs
+    {
+        public int[,] Area { get; set; }
+        public int N { get; set; }
+
+        /* public LifeGamecs(int n)
+         {
+             N = n;
+             Area = new int[n, n];
+         }*/
+        public void GridToArea(DataGridView grid, int n)//из формы будем отправлять таблицу
+        {//смотря на цвет, ставим 0 или 1
+            N = N;
+            Area = new int[n, n];
+
+            for (int col = 0; col < n; col++)
+            {
+                for (int row = 0; row < n; row++)
+                {
+                    if (grid[col, row].Style.BackColor == Color.Black)
+                        Area[row, col] = 1;
+                    else
+                        Area[row, col] = 0;
+                }
+            }
+        }
+        public void AreaToGrid(DataGridView grid)
+        {//ставим цвет в зависимости от цифры
+            for (int col = 0; col < N; col++)
+            {
+                for (int row = 0; row < N; row++)
+                {
+                    if (Area[row, col] == 1)
+                        grid[col, row].Style.BackColor = Color.Black;
+                    else
+                        grid[col, row].Style.BackColor = Color.White;
+                }
+            }
+        }
+        public void NextDay()
+        {
+            int[,] buffer = new int[N, N];
+            int count;
+            for (int col = 0; col < N; col++)
+            {
+                count = 0;
+                for (int row = 0; row < N; row++)
+                {
+                    if (row < N - 1) count += Area[col, row + 1];
+                    if (row >0) count += Area[col, row - 1];
+
+                    if (col < N - 1 && row < N - 1) count += Area[col + 1, row + 1];
+                    if (col < N - 1 && row > 0) count += Area[col + 1, row - 1];
+                    if (col < N - 1) count += Area[col + 1, row];
+
+                    if (col >0 && row > N-1) count += Area[col - 1, row + 1];
+                    if (col >0 - 1 && row >0) count += Area[col - 1, row - 1];
+                    if (col >0) count += Area[col - 1, row];
+
+                    if (Area[col, row] == 0 && count == 3)
+                        buffer[col,row] = 1;//мертвая и 3 соседа - воскресает
+
+                    else if (Area[col, row] == 1 && (count == 2 || count == 3))
+                        buffer[col, row] = 1;//живая и 2 или 3 соседа живет
+
+                    else
+                        buffer[col, row] = 0;//во всех остальных мертва
+
+                }
+            }
+        }
+    }
+}
